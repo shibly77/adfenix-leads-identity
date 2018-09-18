346f4a000b2a4ef497fda67c5c56bc8545a1d040
Shibly <shibly77@outlook.com>
2018-09-18 14:14:03 +0600

    deps prep

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,10 @@ source ../deploy-envs.sh
 #AWS_ACCOUNT_NUMBER={} set in private variable
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
+#Get and copy deps
+git clone https://github.com/AdfenixLeads/adfenix-leads-core.git ../../adfenix-leads-core
+mkdir -p ../app/deps && cp -r ../../adfenix-leads-core ../app/deps
 
-git clone https://github.com/AdfenixLeads/adfenix-leads-core.git adfenix-leads-core
-ls
-cd adfenix-leads-core
-ls
 
 # Build process
 docker build -t $IMAGE_NAME ../
